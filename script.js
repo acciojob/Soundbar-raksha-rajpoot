b684a7b16849c164726f4cd89867a693a05da074
raksha-rajpoot <rakraj654@gmail.com>
2025-04-08 22:36:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const sounds = ['sound1', 'sound2', 'sound3'];
+const audioElements = {};
+
+sounds.forEach((sound, index) => {
+  const audio = new Audio(`sounds/${sound}.mp3`);
+  audioElements[sound] = audio;
+
+  document.querySelectorAll('.btn')[index].addEventListener('click', () => {
+    stopAllSounds();
+    audio.play();
+  });
+});
+
+document.querySelector('.stop').addEventListener('click', () => {
+  stopAllSounds();
+});
+
+function stopAllSounds() {
+  Object.values(audioElements).forEach(audio => {
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
